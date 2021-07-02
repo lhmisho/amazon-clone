09050e3fca8ba9bb8f870d1f6bf09f8fd921a978
lhmisho <lhmisho@gmail.com>
2021-07-02 21:43:29 +0600

    install fontawsm and create addToCart function

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 
 const Product = (props) => {
     const product = props.product
@@ -16,6 +19,7 @@ const Product = (props) => {
                 <p><small>${product.price}</small></p>
                 <br/>
                 <p><small>Only {product.stock} left in stock - Order soon</small></p>
+                <button onClick={() => props.handleAddProduct(product)} className="main-button"><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon> add to cart</button>
             </div>
         </div>
     );
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -8,11 +8,15 @@ const Shop = () => {
     const first10 = fakeData.slice(0,10)
     const [products, setProducts] = useState(first10)
 
+    const handleAddProduct = (product) => {
+        console.log("Product added", product.name)
+    }
+
     console.log(products)
     return (
         <div className="shop-container">
             <div className="product-container">
-                {first10.map(product => <Product product={product}></Product>)}
+                {first10.map(product => <Product handleAddProduct={handleAddProduct} product={product}></Product>)}
             </div>
             <div className="cart-container">
                 <h3>Cart container</h3>
